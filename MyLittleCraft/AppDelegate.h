4f9f5189b2cbf4dcd00277451fe2d4fe759a9c17
huangkun <jack-huang-developer@foxmail.com>
2017-11-04 16:36:39 +0800

    Initial Commit

---
+++ MyLittleCraft/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLittleCraft
+//
+//  Created by huangkun on 2017/11/4.
+//  Copyright © 2017年 huangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLittleCraft/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLittleCraft
+//
+//  Created by huangkun on 2017/11/4.
+//  Copyright © 2017年 huangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
